034918d16aab0003e8823c7ea8b60e1154baccff
ReneLLaM <rene.llanos.com.rl@gmail.com>
2021-09-16 08:27:05 -0400

    Agregar archivos de proyecto.

---
+++ ejemplo01/Prinisipal.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Nombre: Rene Llanos Machuca" << endl;
+	cout << "Carrera: Ingenieria de Sistemas" << endl;
+	cout << "CU:35-5051" << endl;
+	return 0;
+}
